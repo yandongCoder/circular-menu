a389f77fcefe59c1bb403625d386fa9120b11bbb
yandong <470604226@qq.com>
2016-08-01 13:46:06 +0800

    add styles to show method

--- dist/js/circular-menu.js
+++ dist/js/circular-menu.js
@@ -551,7 +551,10 @@
         return this;
     }
 
-    function show () {
+    function show (styles) {
+
+        if(styles) this.styles(styles);
+        
         classed(this._container, 'opened-nav', true);
         return this;
     }
--- src/show.js
+++ src/show.js
@@ -1,5 +1,8 @@
 import classed from "./classed";
-export default function () {
+export default function (styles) {
+
+    if(styles) this.styles(styles);
+    
     classed(this._container, 'opened-nav', true);
     return this;
 }
